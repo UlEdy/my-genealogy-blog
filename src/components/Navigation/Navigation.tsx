a7b5d77f0494feb45d06832e183fa47ae389b56f
Edyta <ulanowicz.edyta@gmail.com>
2023-04-21 15:29:22 +0200

    added nav items; changed colors name

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,3 +1,8 @@
+import { useState } from 'react';
+
+//components
+import NavItem from './NavItem';
+
 //styles
 import {
     NavigationWrapper,
@@ -11,11 +16,27 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 library.add(faBars);
 
 export const Navigation: React.FC = () => {
+    const [showMore, setShowMore] = useState(false);
+    const handleBurgerClick = () => {
+        setShowMore(prevShow => !prevShow);
+    };
     return (
         <NavigationWrapper>
-            <NavigationBurger>
+            <NavigationBurger onClick={handleBurgerClick}>
                 <StyledFontAwesomeIcon icon={faBars} />
             </NavigationBurger>
+            <NavItem
+                isShow={showMore}
+                label='Blog'
+            />
+            <NavItem
+                isShow={showMore}
+                label='Drzewo genealogiczne'
+            />
+            <NavItem
+                isShow={showMore}
+                label='O mnie'
+            />
         </NavigationWrapper>
     );
 };
